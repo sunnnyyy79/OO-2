93eadffaa78c95707422ba3cb76e2aaf537121a1
sunnnyyy79 <55527086+sunnnyyy79@users.noreply.github.com>
2019-11-22 14:12:14 +0800

    Update Homework1.java

--- Homework1.java
+++ Homework1.java
@@ -1,16 +1,35 @@
+abstract class CSharp
+{
+  protected String color;
+  public void setColor(string str)
+  {
+    color=str;
+  }
+  public abstract void show();
+}
+
 class CTriangle extends CShape
 {
   protected double a;
   protected double b;
   protected double c;
-  public CTriangle (int A,int B,int C)
+  public CTriangle (double a,double b,double c)
   {
-    double a = A;
-    double b = B;
-    double c = C;
+    a = A;
+    b = B;
+    c = C;
   }
   public void show()
   {
-  system.out.print("color="+red+", ");
+  system.out.print("color="+color+", ");
   system.out.println("area="+0.5*a*b);
+}
+
+public class homework{
+    public static void main(String args[])
+    {
+        Cshape shape1 = new CTriangle(3,4,5);
+        shape1.setColor("Red");
+        shape1.show();
+    }
 }
