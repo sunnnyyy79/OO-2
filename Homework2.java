a24260b0481edcfb3355960e05788f1fb98a773d
sunnnyyy79 <55527086+sunnnyyy79@users.noreply.github.com>
2019-12-02 17:14:25 +0800

    Update Homework2.java

--- Homework2.java
+++ Homework2.java
@@ -1,43 +1,43 @@
 Interface Shape
 {
-Abstract void getArea()
+  Abstract void getArea()
 }
 
 class Rectangle implements Shape
 {
-double length;
-double width;
-public Rectangle(int w, int l)
-{
-length = l;
-width = w;
-}
+  double length;
+  double width;
+  public Rectangle(int w, int l)
+  {
+    length = l;
+    width = w;
+  }
 
-public void getArea()
-{
-system.out.println(“Rectangle=”+width*length);
-}
+  public void getArea()
+  {
+    system.out.println(“Rectangle=”+width*length);
+  }
 
-Public String toString()
-{
-return “Area = “;
-}
+  Public String toString()
+  {
+  return “Area = “;
+  }
 
 }
 
 class Triangle implements Shape
 {
-double base;
-double height;
-public Rectangle(int b, int h)
-{
-base = b;
-height = h;
-}
+  double base;
+  double height;
+  public Rectangle(int b, int h)
+  {
+    base = b;
+    height = h;
+  }
 
-public void getArea()
-{
-system.out.println(“Triangle=”+base*height*0.5);
-}
+  public void getArea()
+  {
+    system.out.println(“Triangle=”+base*height*0.5);
+  }
 
 }
